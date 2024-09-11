6e8f0fbad216697ac6856fe331774911bb9fca21
alanb4rt <al4n.b4rt@gmail.com>
2024-09-11 17:06:48 +0200

    add semantic tag

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,17 @@ export default function App() {
       <div className="flex items-center min-h-screen">
         <div className="w-full max-w-screen-lg mx-auto p-4">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-            <div className="md:row-span-2 flex flex-col bg-[var(--dark-blue)] rounded-2xl overflow-hidden">
+            <section className="md:row-span-2 flex flex-col bg-[var(--dark-blue)] rounded-2xl overflow-hidden">
               <UserProfile firstName={firstName} lastName={lastName} />
               <TimeframeMenu
                 timeframe={timeframe}
                 setTimeframe={setTimeframe}
               />
-            </div>
+            </section>
             <Dashboard data={data} timeframe={timeframe} />
           </div>
         </div>
       </div>
-      <img
-        className="hidden absolute inset-0 w-full h-screen object-cover"
-        src="./src/design/desktop-design.jpg"
-        alt=""
-      />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,7 @@ export default function Card({ item, timeframe, color }) {
   const iconURL = `icon-${item.title.toLowerCase().replace(" ", "-")}.svg`;
 
   return (
-    <div
+    <section
       className="group relative rounded-2xl pt-9 overflow-hidden"
       style={{ backgroundColor: color }}
     >
@@ -29,12 +29,12 @@ export default function Card({ item, timeframe, color }) {
           <img className="hover:brightness-200" src={iconEllipsis} alt="..." />
         </div>
         <div className="flex justify-between items-center sm:items-stretch sm:flex-col gap-2">
-          <p className="text-2xl sm:text-5xl font-light">{current}hrs</p>
+          <h3 className="text-2xl sm:text-5xl font-light">{current}hrs</h3>
           <p className="text-sm opacity-60 leading-[inherit]">
             {timeframeText} - {previous}hrs
           </p>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
